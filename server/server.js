566c1bd3db655cf17ed26b0e703ed8fd8ec3b323
johnny <nintendof1@gmail.com>
2021-12-26 12:20:50 +0800

    add customer

--- server/server.js
+++ server/server.js
@@ -26,5 +26,41 @@ app.get('/', (req, res) => {
   res.send('Node.js + Express.js + EC');
 });
 
-
+// 客戶註冊/登入
+// Database : EC0507
+// Collection : customers
+// 新增客戶
+app.post('/customers/add',(req,res)=>{
+    let xuserName = req.body.userName;
+    let xeMail = req.body.eMail;
+    let xpassword = req.body.password;
+    // 要建立資料的 Class
+    let xCustomer ={
+        userName: xuserName,
+        eMail: xeMail,
+        password: xpassword,
+        eMailConfirm:false,
+        telphone:'',
+        mobile:'',
+        address:'',
+        personalID=''
+    };
+    MongoClient.connect(MongoURL,(err,db)=>{
+        if (err){
+            console.log("MongoDB Connect error ..." + err);
+        }
+        else{
+            let dbo = db.db("EC0507");
+            dbo.collection('customers').insertOne(xCustomer,(err,result)=>{
+                if (err){
+                    console.log("EC0507.customers -> Error");
+                    }
+                else {
+                    db.close();
+                    res.json(result);
+                }
+            });
+        }
+    })
+})
 
