afb11e021dd1bfe272bbb942f9e1320e56ae8de3
capeta0507 <nintendof1@gmail.com>
2021-12-28 21:47:14 +0800

    server.js login

--- server/server.js
+++ server/server.js
@@ -10,16 +10,16 @@ app.use(express.json());
 // Server listen
 const PORT = 8080;
 app.listen(PORT,()=>{
-    console.log(`Running on http://localhost:${PORT}`);
-    MongoClient.connect(MongoURL,(err,db)=>{
-        if (err){
-          console.log("MongoDB Connect error ..." + err);
-        }
-        else{
-          console.log("MongoDB Connect OK ..." + MongoURL);
-          db.close();
-        }
-    });
+  console.log(`Running on http://localhost:${PORT}`);
+  MongoClient.connect(MongoURL,(err,db)=>{
+		if (err){
+			console.log("MongoDB Connect error ..." + err);
+		}
+		else{
+			console.log("MongoDB Connect OK ..." + MongoURL);
+			db.close();
+		}
+  });
 });
 
 app.get('/', (req, res) => {
@@ -31,35 +31,54 @@ app.get('/', (req, res) => {
 // Collection : customers
 // 新增客戶
 app.post('/customers/add',(req,res)=>{
-    const{userName,eMail,password} = req.body;
-    // TODO : password 要加密，在儲存進資料庫
-    // 要建立資料的 Class
-    let xCustomer ={
-        userName: userName,
-        eMail: eMail,
-        password: password,
-        eMailConfirm:false,
-        telphone:'',
-        mobile:'',
-        address:'',
-        personalID:''
-    };
-    MongoClient.connect(MongoURL,(err,db)=>{
-        if (err){
-            console.log("MongoDB Connect error ..." + err);
-        }
-        else{
-            let dbo = db.db("EC0507");
-            dbo.collection('customers').insertOne(xCustomer,(err,result)=>{
-                if (err){
-                    console.log("EC0507.customers -> Error");
-                    }
-                else {
-                    db.close();
-                    res.json(result);
-                }
-            });
-        }
-    })
-})
+	const{userName,eMail,password} = req.body;
+	// TODO : password 要加密，在儲存進資料庫
+	// 要建立資料的 Class
+	let xCustomer ={
+		userName: userName,
+		eMail: eMail,
+		password: password,
+		eMailConfirm:false,
+		telphone:'',
+		mobile:'',
+		address:'',
+		personalID:''
+	};
+	MongoClient.connect(MongoURL,(err,db)=>{
+		if (err){
+				console.log("MongoDB Connect error ..." + err);
+		}
+		else{
+			let dbo = db.db("EC0507");
+			dbo.collection('customers').insertOne(xCustomer,(err,result)=>{
+				if (err){
+					// 新增錯誤
+					res.json({
+						success : false,
+						message:"/customers/add ... 新增錯誤",
+						error : err                                                              
+					});
+				}
+				// 新增OK
+				else {
+					db.close();
+					res.json({
+						success:true,
+						message:"/customers/add ... 新增OK",
+						result : result
+					});
+				}
+			});
+		}
+	})
+});
+
+// TODO : 新增登入
+// Database : EC0507
+// Collection : customers -> userName , eMail , password
+app.post('/customers/login',(req,res)=>{
+	// 根據前端 eMail + password 讀取資料來判斷帳密是否正確
+	// 回傳 _id , userName
+});
+
 
