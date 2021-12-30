9ab0f28bf22ea553763bd302c0c715c4b42f2737
詹政勳 <nintendof1@gmail.com>
2021-12-30 22:14:32 +0800

    password

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.static(__dirname + '/public')); //主機資源設定為 public 資料夾
 app.use(express.json());
 
+// 加密演算
+const crypto = require('crypto');
+let myIv = 'd55daa8fb6a348ee';  // 16位元 IV
+let myKEY = '0123456789-0123456789-0123456789'  // 32位元 使用者自訂
+let myAlgorithm = 'aes-256-cbc'  // 加密演算法
+
 // Server listen
 const PORT = 8080;
 app.listen(PORT,()=>{
@@ -33,14 +39,22 @@ app.listen(PORT,()=>{
 app.post('/customers/add',(req,res)=>{
 	const{userName,eMail,password} = req.body;
 	// TODO : 
-	//   1. password 要加密，在儲存進資料庫
+	//   1. password 要加密，在儲存進資料庫  okˇ
 	//   2. eMail 不應該重複
+
+  // 加密處理
+  let cipher = crypto.createCipheriv(myAlgorithm,myKEY,myIv);
+  let encrypted = cipher.update(password, 'utf-8', 'hex');
+  encrypted += cipher.final('hex');
+
+  // console.log('加密前', password);
+  // console.log('加密後', encrypted);
 
 	// 要建立資料的 Class
 	let xCustomer ={
 		userName: userName,
 		eMail: eMail,
-		password: password,
+		password: encrypted,
 		eMailConfirm:false,
 		telphone:'',
 		mobile:'',
@@ -114,7 +128,15 @@ app.post('/customers/login',(req,res)=>{
 										return false;
 									}
 
-									if (result[0].password !== password) {
+                  // 解密處理
+                  let decipher = crypto.createDecipheriv(myAlgorithm,myKEY,myIv);
+                  let decrypted = decipher.update(result[0].password , 'hex', 'utf-8');
+                  decrypted += decipher.final('utf-8');
+
+                  // console.log('解密前', result[0].password);
+                  // console.log('解密後', decrypted);
+
+									if (decrypted !== password) {
 										// console.log('密碼錯誤');
 										res.json({
 											success:false,
