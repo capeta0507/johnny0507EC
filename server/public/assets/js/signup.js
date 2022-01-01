e59f92eeb298b1740e5f220be8300771752edddc
詹政勳 <nintendof1@gmail.com>
2022-01-01 21:55:03 +0800

    sign

--- server/public/assets/js/signup.js
+++ server/public/assets/js/signup.js
@@ -33,27 +33,36 @@ $('#mySubmit').on('click', function(){
     let myEmail = $('#myEmail').val();
     let myPassword = $('#myPassword').val();
     let myPasswordCheck = $('#myPasswordCheck').val();
+
+    $('.form-control').removeClass('sign_error');
+    $('#myErrTxt').html('');
     // 輸入姓名
     if(myName == ""){
         $('#myErrTxt').text('請輸入您的姓名');
+        $('#myName').addClass('sign_error');
         return false
     }
     // email 格式
     if(myEmail == ""){
         $('#myErrTxt').text('請輸入電子郵件');
+        $('#myEmail').addClass('sign_error');
         return false
     }
     if(myEmail.search(emailRule)){
         $('#myErrTxt').text('您輸入的email格式有誤');
+        $('#myEmail').addClass('sign_error');
         return false
     }
     // 密碼確認
     if(myPassword == ""){
         $('#myErrTxt').text('請輸入密碼');
+        $('#myPassword').addClass('sign_error');
         return false
     }
     if(myPassword !== myPasswordCheck){
         $('#myErrTxt').text('您設定的密碼跟驗證的不相符喔！');
+        $('#myPassword').addClass('sign_error');
+        $('#myPasswordCheck').addClass('sign_error');
         return false
     }
 
@@ -71,12 +80,18 @@ $('#mySubmit').on('click', function(){
     axios.post('/customers/add', SignUpData)
         .then(res => {
             if (res.data.success == false){
+                // 用alert
                 alert(res.data.message);
+                $('#myEmail').addClass('sign_error');
+                // 用text
+                $('#myErrTxt').text(res.data.message);
                 return false;
             }
             console.log(res.data);
-            alert("恭喜您完成註冊！請至首頁登入喔！");
-            window.location.href = "login.html";
+            setTimeout(() => {
+                alert("恭喜您完成註冊！請至首頁登入喔！");
+                window.location.href = "login.html";
+            },50);
         })
         .catch(error => {
             console.log(error.response);
