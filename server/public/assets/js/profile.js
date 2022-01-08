06d7f582df6c048d9899c397d75475e4429982b8
詹政勳 <nintendof1@gmail.com>
2022-01-08 12:23:19 +0800

    dssaa

--- server/public/assets/js/profile.js
+++ server/public/assets/js/profile.js
@@ -40,40 +40,91 @@ $('#myChange').on('click', function(){
 
     let userEmail = sessionStorage.getItem('eMail');
       if (!userEmail){
-        alert('授權錯誤，必須先登入');
+        $('#myErrTxt').text('請輸入舊密碼');
         // window.location.href = "login.html";
         return false;
     }
 
     $('.form-control').removeClass('sign_error');
+    $('#myErrTxt').html('');
     if(!myOldPassword){
-        alert('請輸入舊密碼')
+        $('#myErrTxt').text('請輸入舊密碼');
+        $('#myOldPassword').addClass('sign_error');
+        return false
     }
     if(!myPassword){
-        alert('請輸入新密碼')
+        $('#myErrTxt').text('請輸入新密碼');
+        $('#myPassword').addClass('sign_error');
+        return false
     }
     if(myPassword !== myPasswordCheck){
-        alert('您設定的密碼跟驗證的不相符喔！')
+        $('#myErrTxt').text('您設定的密碼跟驗證的不相符喔！');
+        $('#myPasswordCheck').addClass('sign_error');
+        return false
+    }
+
+    if(myPassword == myOldPassword){
+        $('#myErrTxt').text('您設定的密碼不可以跟原密碼一樣喔！');
+        $('#myOldPassword').addClass('sign_error');
+        $('#myPassword').addClass('sign_error');
+        return false
     }
     let upDatePassword = {
         password: myPassword
     }
     // console.log('upDatePassword', upDatePassword)
-    axios.put(`/customers/member/${userEmail}`, upDatePassword)
-    .then(res=>{
-        console.log(res.data);
-        if (res.data.success == true){
-            sessionStorage.removeItem('userName');
-            sessionStorage.removeItem('eMail');
-            sessionStorage.removeItem('id');
-            alert("修改完成請重新登入！");
-            window.location.href = "login.html";
+
+    let loginData = {
+        eMail: userEmail,
+        password: myOldPassword
+    }
+
+    axios.post('/customers/login', loginData)
+        .then(res=>{
+            console.log(res.data);
+            if (res.data.success === false){
+                alert('授權錯誤 請重新登入')
+                return false
+            }
+            axios.put(`/customers/member/${userEmail}`, upDatePassword)
+            .then(res=>{
+                console.log(res.data);
+                if (res.data.success == true){
+                    $('.form-control').removeClass('sign_error');
+                    sessionStorage.removeItem('userName');
+                    sessionStorage.removeItem('eMail');
+                    sessionStorage.removeItem('id');
+                    alert("修改完成請重新登入！");
+                    window.location.href = "login.html";
+                    
+                }else {
+                    alert('您的密碼設置錯誤 : ' + res.data.message);
+                }
+            })
+            .catch(error => {
+                console.log(error.response);
+            });
+        })
+        .catch(error => {
+            console.log(error.response);
+            alert('您輸入的帳號密碼有誤！')
+        });
+
+    // axios.put(`/customers/member/${userEmail}`, upDatePassword)
+    // .then(res=>{
+    //     console.log(res.data);
+    //     if (res.data.success == true){
+    //         sessionStorage.removeItem('userName');
+    //         sessionStorage.removeItem('eMail');
+    //         sessionStorage.removeItem('id');
+    //         alert("修改完成請重新登入！");
+    //         window.location.href = "login.html";
             
-        }else {
-            alert('您的密碼設置錯誤 : ' + res.data.message);
-        }
-    })
-    .catch(error => {
-        console.log(error.response);
-    });
+    //     }else {
+    //         alert('您的密碼設置錯誤 : ' + res.data.message);
+    //     }
+    // })
+    // .catch(error => {
+    //     console.log(error.response);
+    // });
 })
