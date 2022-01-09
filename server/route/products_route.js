45751df86075da074d4e4fbbb86da779422cdc31
詹政勳 <nintendof1@gmail.com>
2022-01-09 22:18:12 +0800

    sda

--- server/route/products_route.js
+++ server/route/products_route.js
@@ -4,10 +4,18 @@ require('dotenv').config('../.env'); // 引用 .env 環境變數
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const MongoURL = process.env.MONGODB_URL
+// 借用
+const products = require('../public/shop/product/product.json');
 
 // 產品讀取
 router.get('/all',(req,res)=>{
-  res.send('Product read all ...');
+  // res.send('Product read all ...');
+  // console.log(products)
+  res.json({
+    success:true,
+    message:"成功讀取商品資料",
+    result:products
+  });
 });
 
 module.exports = router;
