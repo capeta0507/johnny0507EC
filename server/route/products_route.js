aafa586bee5ce8bde181a95492f0886bd7dba7b7
capeta0507 <nintendof1@gmail.com>
2022-01-12 23:00:39 +0800

    products

--- server/route/products_route.js
+++ server/route/products_route.js
@@ -8,14 +8,43 @@ const MongoURL = process.env.MONGODB_URL
 const products = require('../public/shop/product/product.json');
 
 // 產品讀取
-router.get('/all',(req,res)=>{
-  // res.send('Product read all ...');
-  // console.log(products)
-  res.json({
-    success:true,
-    message:"成功讀取商品資料",
-    result:products
-  });
+router.get('/category/:cat',(req,res)=>{
+  let myCat = req.params.cat;
+  // console.log(myCat);
+  // 搜尋條件
+  let xCategory = {
+    category: myCat,
+  };
+  // 參數 All 代表全部
+  if (myCat == 'All'){
+    xCategory = {
+      // 全部
+    }
+  }
+  // console.log(xCategory);
+  // 讀取資料
+  MongoClient.connect(MongoURL,(err,db)=>{
+    if (err){
+			console.log("MongoDB Connect error ..." + err);
+		} else{
+      let dbo = db.db("EC0507");
+			dbo.collection("products").find(xCategory).toArray((err,result)=>{
+        if(err){
+          res.json({
+            success:false,
+            message:"/category/:cat ... 找尋錯誤",
+          });
+        }else {
+          db.close();
+          res.json({
+            success:true,
+            message:"成功讀取商品資料",
+            result:result
+          });
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
