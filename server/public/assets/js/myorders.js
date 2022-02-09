b2b2d299f8b388d8a758f2d62ee8619b7736b38b
詹政勳 <nintendof1@gmail.com>
2022-02-09 10:40:11 +0800

    order

--- server/public/assets/js/myorders.js
+++ server/public/assets/js/myorders.js
@@ -1,3 +1,84 @@
 // let myNo = 'ECx20220207153113000';
 // let myNo2 = myNo.slice(3,11)
 // console.log(myNo2)
+// let myYear = myNo2.slice(0, 4)
+// let myMonth = myNo2.slice(4, 6)
+// let myDate = myNo2.slice(6, 8)
+// let order_date = myYear + '/' + myMonth + '/' + myDate
+// console.log(order_date)
+
+let myOrder = ''
+let myEmail = sessionStorage.getItem('eMail');
+// console.log('myEmail', myEmail)
+
+axios.get(`/customers/myorders/${myEmail}`)
+  .then(result=>{
+    if (result.data.success == true){
+     result.data.result.map((data, oerder_key)=>{
+      let myDateString = data.orderNo.slice(3,11);
+      let myYear = myDateString.slice(0, 4);
+      let myMonth = myDateString.slice(4, 6);
+      let myDate = myDateString.slice(6, 8);
+      let order_date = myYear + '/' + myMonth + '/' + myDate;
+      // console.log(oerder_key)
+      // console.log(data)
+      myOrder += `
+        <table class="table table-bordered table_list">
+          <thead class="table-secondary">
+            <tr>
+              <th class="text-center" scope="col">訂單編號</th>
+              <th class="text-center" scope="col">日期</th>
+              <th class="text-center" scope="col">名稱</th>
+              <th class="text-center" scope="col">總價</th>
+            </tr>
+          </thead>
+          <tbody id="order_list">
+            <tr>
+              <th class="text-center" scope="row">${data.orderNo}</th>
+              <td class="text-center">${order_date}</td>
+              <td class="text-center">${data.description}</td>
+              <td class="text-center">NT$ ${data.amt}</td>
+            </tr>
+          </tbody>
+        </table>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample${oerder_key}" aria-expanded="false" aria-controls="collapseExample${oerder_key}">
+          明細
+        </button>
+        <div class="collapse" id="collapseExample${oerder_key}">
+            <div class="shop_array_order">
+              <table id="myShopArray" class="table table-bordered table-hover table-dark">
+                <thead>
+                  <tr>
+                    <th class="text-center" scope="col">商品編號</th>
+                    <th class="text-center" scope="col">商品名稱</th>
+                    <th class="text-center" scope="col">數量</th>
+                    <th class="text-center" scope="col">價格</th>
+                  </tr>
+                </thead>
+                <tbody id="shop_list">
+      `
+      data.shopArray.map((shop_data)=>{
+        myOrder += `
+          <tr>
+            <td class="text-center">${shop_data.no}</td>
+            <td class="text-center">${shop_data.name}</td>
+            <td class="text-center">${shop_data.qty}件</td>
+            <td class="text-center">NT$ ${shop_data.total}</td>
+          </tr>
+        `
+      })
+      myOrder += `
+              </tbody>
+            </table>
+          </div>
+        </div>
+      `
+      $('#myorders').html(myOrder);
+     })
+    } else {
+      console.log('查無資料');
+    }
+  })
+  .catch(err =>{
+    console.log(err.message);
+  })
